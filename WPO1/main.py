372b7045ebeab72a313d883638a6335ed00b5c11
leandro-svg <leandro.dibella@gmail.com>
2022-11-30 14:32:08 +0100

    First lab

--- WPO1/main.py
+++ WPO1/main.py
@@ -8,16 +8,11 @@ from matplotlib import pyplot as plt
 import os
 
 def click_event(event, x, y, flags, params):
- 
-    # checking for left mouse clicks
+    print("main side")
     if event == cv2.EVENT_LBUTTONDOWN:
  
-        # displaying the coordinates
-        # on the Shell
         print(x, ' ', y)
  
-        # displaying the coordinates
-        # on the image window
         font = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(image, str(x) + ',' +
                     str(y), (x,y), font,
@@ -27,6 +22,22 @@ def click_event(event, x, y, flags, params):
             f.write(str(x) + ',' + str(y))
             f.write('\n')
         cv2.imshow('image', image)
+
+def click_event_left(event, x, y, flags, params):
+    print("left side")
+    if event == cv2.EVENT_LBUTTONDOWN:
+ 
+        print(x, ' ', y)
+ 
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(image_left, str(x) + ',' +
+                    str(y), (x,y), font,
+                    1, (255, 0, 0), 2)
+
+        with open('Inputs/image_coordinate_left.txt', 'a') as f:
+            f.write(str(x) + ',' + str(y))
+            f.write('\n')
+        cv2.imshow('image_left', image_left)
  
 
 class Calibration():
@@ -37,16 +48,21 @@ class Calibration():
         image = cv2.imread(img_path)
         return image
 
-    def getPointsFromImage(self, image):
+    def getPointsFromImage(self, image, path, choice):
 
-        if not(os.path.isfile("Inputs/image_coordinate.txt")):
+        if not(os.path.isfile(path)):
             cv2.imshow('image', image)
-            cv2.setMouseCallback('image', click_event)
+            if (choice == 0):
+                cv2.imshow('image', image)
+                cv2.setMouseCallback('image', click_event)
+            elif (choice == 1 ):
+                cv2.imshow('image_left', image)
+                cv2.setMouseCallback('image_left', click_event_left)
             cv2.waitKey(1000000)
             cv2.destroyAllWindows()
         else:
             print("Image Coordinate file already exists, we will work with it")
-        with open('Inputs/image_coordinate.txt', 'r') as file:
+        with open(path, 'r') as file:
             lines = [line.rstrip() for line in file]
 
         image_coord = []
@@ -109,7 +125,9 @@ class Calibration():
         
         extrinsic = np.concatenate((R, np.array([T]).T), axis=1)
         added = np.array([[0,0,0,1]])
-        extrinsic = np.concatenate((extrinsic, added))
+        extrinsic_add = np.concatenate((extrinsic, added))
+        
+        print("MMMMMM3", np.dot(intrinsic, extrinsic))
         camParameters = {}
         
         camParameters["ImageCoordinate"] = image_coord
@@ -121,6 +139,30 @@ class Calibration():
         camParameters["TranslationMatrix"] = T
         print(camParameters)
         return camParameters
+    
+    def leftImage(self, image_left,path,  camParameters):
+        image_points_left_temp = self.getPointsFromImage(image_left, "Inputs/image_coordinate_left.txt", 1)
+        
+        image_coord_left = np.array([])
+        for elem in image_points_left_temp:
+            elem = np.append(elem, [1])
+            image_coord_left = np.append(image_coord_left, [elem])
+        image_coord_left = np.reshape(image_coord_left, (np.shape(image_points_left_temp)[0],3))
+        
+        world2image = np.dot(camParameters["Intrinsic"],camParameters["Extrinsic"])
+        M = np.zeros((3,3))
+        for i in range(0,3):
+            M[i,0] = world2image[i,0]
+            M[i,1] = world2image[i,1]
+            M[i,2] = world2image[i,3]
+        print(M)
+        image2world = np.linalg.inv(M)
+        world_coord_left = np.dot(image2world,  image_coord_left.T)
+        print("Those are world coordinate")
+        print(world_coord_left)
+    
+    
+    
 def get_Parser():
     parser = argparse.ArgumentParser(
             description="Implementation of the required Calibration")
@@ -129,6 +171,12 @@ def get_Parser():
             default="Inputs/calibration_pointnumbering.jpg",
             type=str,
             help="Directory to input images",
+            )
+    parser.add_argument(
+            "--left",
+            default="Inputs/left.jpg",
+            type=str,
+            help="Directory to left input images",
             )
 
     parser.add_argument(
@@ -143,15 +191,18 @@ def get_Parser():
 if __name__ == '__main__':
     args = get_Parser().parse_args()
     img_path = args.input
+    img_path_left = args.left
 
     world_coord_file = args.txtfile
     Calibration = Calibration()
     
     image = Calibration.PreProcess(img_path)
     
-    image_points = Calibration.getPointsFromImage(image)
+    image_points = Calibration.getPointsFromImage(image, 'Inputs/image_coordinate.txt', 0)
 
     M, image_coord, world_coord = Calibration.projectMatrix(image_points, world_coord_file)
     
     camParameters = Calibration.getCameraParameters(M, image_coord, world_coord)
     
+    image_left = Calibration.PreProcess(img_path_left)
+    Calibration.leftImage(image_left,img_path_left,  camParameters)
