4667bb34ed313897047052faf7bd2386b6906cd5
leandro-svg <leandro.dibella@gmail.com>
2022-12-13 18:14:51 +0100

    3D world

--- OpticalFlow/main.py
+++ OpticalFlow/main.py
@@ -5,6 +5,7 @@ import cv2 as cv
 import tqdm
 import os
 import glob
+import time
 from random import randint
 from skimage.transform import warp, ProjectiveTransform
 from mpl_toolkits import mplot3d
@@ -42,13 +43,15 @@ class OpticalFlow():
         if len(image_path) == 1:
             image_path = glob.glob(os.path.expanduser(image_path[0]))
             assert image_path, "The input path(s) was not found"
-            
         images = []
         for img_path in tqdm.tqdm(image_path):
             img = cv2.imread(img_path)
             images.append(img)
             real_image = img.copy()
-            
+        fig = plt.figure(figsize= (10, 10))
+        fig2 = plt.figure(figsize= (10, 10))
+        fig3 = plt.figure(figsize= (10, 10))
+
         for index in range(1,len(images)):
 
             img1 = cv2.imread(image_path[index-1], cv2.IMREAD_GRAYSCALE)
@@ -56,6 +59,9 @@ class OpticalFlow():
             img1 = img1/255
             img2 = img2/255
             img_1_x = cv2.Sobel(img1, cv2.CV_64F, 1, 0, ksize=3, borderType=cv.BORDER_DEFAULT)
+            cv2.imwrite('ok.jpg', img_1_x)
+            filter_x = np.transpose(np.array([[-1., -1.], [1., 1.]]))
+            
             img_1_y = cv2.Sobel(img1, cv2.CV_64F, 0, 1, ksize=3, borderType=cv.BORDER_DEFAULT)
             img_2_x = cv2.Sobel(img2, cv2.CV_64F, 1, 0, ksize=3, borderType=cv.BORDER_DEFAULT)
             img_2_y = cv2.Sobel(img2, cv2.CV_64F, 0, 1, ksize=3, borderType=cv.BORDER_DEFAULT)
@@ -64,17 +70,15 @@ class OpticalFlow():
             img2 = cv2.GaussianBlur(img2, (5, 5), 0)
             
             kernel_t = np.array([[1., 1.], [1., 1.]])
-            Ix =  img_1_x # + img_2_x
-            Iy = img_1_y # + img_2_y
+            Ix =  img_1_x + img_2_x
+            Iy = img_1_y + img_2_y
             
             mode = 'same'
-            # It = img1 - img2 
             It = signal.convolve2d(img2, kernel_t, boundary='symm', mode=mode) + signal.convolve2d(img1, - kernel_t, boundary='symm', mode=mode)
             avg_u,avg_v,u,v, iteration  = 0,0,0,0,0
             avg_u = u
             avg_v = v
             while iteration < 1e2:
-                
                 u = avg_u - (Ix*avg_u + Iy*avg_v + It)*Ix/(1+ 1*(Ix ** 2 + Iy ** 2))
                 v = avg_v - (Ix*avg_u + Iy*avg_v + It)*Iy/(1+ 1*(Ix ** 2 + Iy ** 2))
                 kernel = np.ones((2,2),np.float32)/25
@@ -84,22 +88,29 @@ class OpticalFlow():
                 iteration += 1
                 
             flow = flow_uv_to_colors(u,v)
-            plt.imshow(flow)
-            plt.show()
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-
-            
-       
-
-    
+            ax = fig.add_subplot(3, 3, index)
+            ax.imshow(flow)
+            ax2 = fig2.add_subplot(3, 3, index)
+            ax2.imshow(v)
+            ax3 = fig3.add_subplot(3, 3, index)
+            ax3.imshow(u)
+        # plt.show()
         
+        
+        #KANADE EQUATION
+        # try least squares fit
+        # Compute A matrixes
+        
+        
+        # plt.subplot(1, 2, 2)
+        # plt.imshow(v, cmap = 'gray')
+    
 def get_Parser():
     parser = argparse.ArgumentParser(
             description="Implementation of the required Calibration")
     parser.add_argument(
         "--input",
-        default="/home/leand/ULB_course/ComputerVision/WPO2/Basketball/frame1.png",
+        default="./Basketball/*",
         nargs="+",
         help="A file or directory of your input dat",
         )
@@ -110,6 +121,6 @@ def get_Parser():
 if __name__ == '__main__':
     args = get_Parser().parse_args()
     img_path = args.input
-    
+    print(img_path)
     OF = OpticalFlow()
     OF.preProcess(img_path)
