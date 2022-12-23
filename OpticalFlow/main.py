c311ace00fa03af4798c77b1ef9d4fa40f9a3d5b
leandro-svg <leandro.dibella@gmail.com>
2022-12-23 16:40:04 +0100

    3D world

--- OpticalFlow/main.py
+++ OpticalFlow/main.py
@@ -38,23 +38,26 @@ class OpticalFlow():
         Iy = img_1_y + img_2_y
         
         mode = 'same'
-        It = signal.convolve2d(img2, kernel_t, boundary='symm', mode=mode) + signal.convolve2d(img1, - kernel_t, boundary='symm', mode=mode)
+        It = signal.convolve2d(img2, kernel_t, boundary='symm', mode=mode)+ signal.convolve2d(img1, - kernel_t, boundary='symm', mode=mode)
         
         return Ix, Iy, It, img_1, img1, img2
     
     def Horn_Shunck_OF(self, fig, images, image_path):
         print(('Horn-Shunck Optical Flow'))
         print("Computing...")
+        max_iter_list = [1, 1, 5, 10, 50, 100,500]
         for index in tqdm.tqdm(range(1,len(images))):            
             Ix, Iy, It, img_1, img1, img2 = self.computeFeature(image_path, index)
             
             avg_u,avg_v,u,v, iteration  = 0,0,0,0,0
             avg_u = u
             avg_v = v
-            while iteration < 1e2:
-                u = avg_u - (Ix*avg_u + Iy*avg_v + It)*Ix/(1+ 1*(Ix ** 2 + Iy ** 2))
-                v = avg_v - (Ix*avg_u + Iy*avg_v + It)*Iy/(1+ 1*(Ix ** 2 + Iy ** 2))
-                kernel = np.ones((2,2),np.float32)/25
+            lamb = 20
+            max_iter =1
+            while iteration < max_iter:
+                u = avg_u - (Ix*avg_u + Iy*avg_v + It)*Ix/(1+ lamb*(Ix ** 2 + Iy ** 2))
+                v = avg_v - (Ix*avg_u + Iy*avg_v + It)*Iy/(1+ lamb*(Ix ** 2 + Iy ** 2))
+                kernel = np.ones((3,3),np.float32)/9
                 avg_u = cv.filter2D(u,-1,kernel)
                 avg_v = cv.filter2D(v,-1,kernel)
                 iteration += 1
@@ -63,14 +66,14 @@ class OpticalFlow():
             ax = fig.add_subplot(3, 3, index)
             
             ax.imshow(flow)
-            ax.set_title('Horn-Shunck optical Flow')
+            ax.set_title("Horn-Shunck optical Flow with max_iter = %i" % max_iter)
             
         return flow
     
     def lucas_kanade_OF(self, fig, images, image_path):
         print('Lucas Kanade Optical Flow')
         print("Computing...")
-        for index in tqdm.tqdm(range(1,len(images))):            
+        for index in tqdm.tqdm(range(1,len(images)-1)):            
             Ix, Iy, It, img_1, img1, img2 = self.computeFeature(image_path, index)
             
             u = np.zeros(img1.shape)
@@ -90,11 +93,12 @@ class OpticalFlow():
                     IyIt = -1*np.sum(np.multiply(Iy[i - n:i+n+1, j - n:j + n + 1], It[i - n:i+n+1, j - n:j + n + 1]))
                     P = np.array([[IxIt],[IyIt]])
                     uv = np.matmul(A_inv, P)
-                    u[i, j] = uv[0]
-                    v[i, j] = uv[1]
+                    u[i, j] = uv[0]/30
+                    v[i, j] = uv[1]/30
             n, m = u.shape
             flow = flow_uv_to_colors(u,v)
             ax6 = fig.add_subplot(3, 3, index)
+      
             
             ax6.imshow(flow)
             ax6.set_title('Lucas Kanade Optical Flow')
@@ -115,13 +119,13 @@ class OpticalFlow():
         return images
 
     def main(self, images, image_path ):
-        fig = plt.figure(figsize= (10, 10))
-        fig6 = plt.figure(figsize= (10, 10))
-
-        Horn_Shunck_OF = self.Horn_Shunck_OF(fig, images, image_path)
+        fig = plt.figure(figsize= (15, 15))
+        fig6 = plt.figure(figsize= (15, 15))
+        
+        Horn_Shunck_OF = self.Horn_Shunck_OF(fig,images, image_path)
         fig.suptitle("Horn-Shunck Optical Flow", fontsize=15)
         
-        Lucas_Kanade_OF = self.lucas_kanade_OF(fig6, images, image_path)
+        Lucas_Kanade_OF = self.lucas_kanade_OF(fig6,  images, image_path)
         fig6.suptitle("Lucas Kanade Optical Flow", fontsize=15)
         
         fig.savefig('output/Horn_Shunck_OF_demoframes.png')
